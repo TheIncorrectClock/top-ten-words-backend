184d837a1cf6d824d32db0b9b71643b6f4aad0b6
Bartosz Wiśniewski <theincorrectclock@gmail.com>
2019-09-14 19:14:12 +0100

    added cors policy

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const loader = require('./loader');
 const parser = require('./parser');
 
@@ -7,14 +9,18 @@ const port = 8443;
 
 app.get('/top10wordsB/:twoLetters', (req, res) => {
     if (req.params['twoLetters'].length !== 2) {
+        res.header("Access-Control-Allow-Origin", "*");
         res.status(412).json({'error': 'path param should have length 2'});
     } else {
         parser.parse_data(loader.load_content('christmas_carol.txt'), req.params['twoLetters'])
             .subscribe(body => {
+                res.header("Access-Control-Allow-Origin", "*");
                 res.status(200).json(body);
             });
     }
 });
+
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`backend started on port ${port}`);

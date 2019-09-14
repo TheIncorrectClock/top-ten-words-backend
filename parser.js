3573d593e51764c7ad55d632ec9f5ebbd5d0d8c2
Bartosz Wiśniewski <theincorrectclock@gmail.com>
2019-09-14 18:08:26 +0100

    app searches now for top 10 words

--- parser.js
+++ parser.js
@@ -3,14 +3,22 @@ const rxjs_op = require('rxjs/operators');
 
 
 function parse_data(data, twoLetters) {
-    return rxjs.from(data.toString().split(/(\s+)/)).pipe(
-        rxjs_op.filter(word => word.trim().length > 0),
-        rxjs_op.map(word => word.toLowerCase().replace(/[.!?",;:]/g, '')),
-        rxjs_op.filter(word => word.startsWith(twoLetters)),
-        rxjs_op.groupBy(word => word),
-        rxjs_op.mergeMap(group => group.pipe(rxjs_op.reduce((acc, val, index) => [index + 1, val]))),
-        rxjs_op.reduce((acc, val) => [...acc, val])
-    );
+    return rxjs.from(data.toString().split(/(\s+|--)/))
+        .pipe(
+            rxjs_op.filter(word => word.trim().length > 0),
+            rxjs_op.map(word => word.toLowerCase().replace(/[.!?",;:]/g, '')),
+            rxjs_op.filter(word => word.startsWith(twoLetters)),
+            rxjs_op.groupBy(word => word),
+            rxjs_op.mergeMap(group =>
+                group.pipe(
+                    rxjs_op.reduce((acc, val, index) => {
+                        return {count: index + 1, value: val}
+                    }, {}))),
+            rxjs_op.reduce((acc, val) => [...acc, val], []),
+        ).pipe(
+            rxjs_op.map(array => array.sort((a, b) => a.count > b.count ? -1 : 1)),
+            rxjs_op.map(array => array.splice(0, 10))
+        )
 }
 
 module.exports = {
